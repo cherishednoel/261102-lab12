858c33fe9a08d277b5addfbb86b228c823e88756
cherishednoel <panida.stp@icloud.com>
2023-01-08 23:50:29 +0700

    exercise10_1.cpp not complete

---
+++ exercise10_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int x, int y, int z) {
+    int i = 2, ans = 1;
+    while(true) {
+        if(i > x || i > y || i > z) {
+            return ans;
+        }
+        if(x % i == 0 && y % i == 0 && z % i == 0) {
+            ans++;
+            i++;
+        } else {
+            return ans;
+        }
+    }
+}
+
+int main() {
+    cout << "gcd(18,17,16) = " << gcd(18,17,16) << '\n';
+    cout << "gcd(480,320,1024) = " << gcd(480,320,1024) << '\n';
+    cout << "gcd(255,51,765) = " << gcd(255,51,765) << '\n';
+    cout << "gcd(15,1,9) = " << gcd(15,1,9) << '\n';
+    cout << "gcd(2,6,222) = " << gcd(2,6,222) << '\n';
+    return 0;
+}
